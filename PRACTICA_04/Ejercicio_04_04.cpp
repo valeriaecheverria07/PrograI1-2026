4116602f6a5eee7bd62cedbe6c9f626bd1d49fe0
valeriaecheverria07 <valeria.echeverria@ucb.edu.bo>
2026-03-11 15:22:33 -0400

    Add files via upload

--- PRACTICA_04/Ejercicio_04_04.cpp
+++ PRACTICA_04/Ejercicio_04_04.cpp
@@ -1,3 +1,9 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Valeria Fernanda Echeverria Ureña
+// Carnet: 9870105
+// Carrera del estudiante: Ingenieria Biomedica
+// Fecha creación: 11/03/2026
+
 #include <iostream>
 using namespace std;
 
